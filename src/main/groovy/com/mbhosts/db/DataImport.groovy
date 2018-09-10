ea20e169f59614b5970bf603677a6a7ee0a2a50a
Mateus Duarte Jr <mateus.duarte@gridnet.com.br>
2018-09-10 19:02:58 -0300

    static void initialCharge()

--- src/main/groovy/com/mbhosts/db/DataImport.groovy
+++ src/main/groovy/com/mbhosts/db/DataImport.groovy
@@ -7,7 +7,7 @@ import com.mbhosts.security.UserRole
 
 class DataImport {
 
-    def static initialCharge() {
+    static void initialCharge() {
         if (User.count() == 0) {
             def adminRole = new Role(authority: 'ROLE_ADMIN', name: 'Administrador', profileType: ProfileType.ADMIN, value: 1).save()
             def adminUser = new User(name: 'Administrador', username: 'admin@admin.com', password: 'admin', ).save()
